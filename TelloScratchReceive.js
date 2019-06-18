d67a78dfea729eeaa3b47bcff01a84ea4af30cb5
tom <f41ardu@aror.de>
2019-06-18 18:13:22 +0200

    TelloScratchSimple and TelloScratchResonse is OK. Others will be removed.

--- TelloScratchReceive.js
+++ TelloScratchReceive.js
@@ -31,46 +31,19 @@
    var HOST = '192.168.10.1'; // Tello IP
    //var HOST = '127.0.0.1'; // Test localhost (debug mode)
    
-   // Scratch listener port 
-   var listenerPort = 8890; 
-   var listenerHOST = '0.0.0.0';
+  
+  
 
    // udp connector  
    var dgram = require('dgram');
    // client connector (send commands) 
    var client = dgram.createSocket('udp4');
-   // server connector (receive Telle response) 
-   var server1 = dgram.createSocket('udp4');
+   
    
    // initial variables
    var myStatus = 1; // initially set status to yellow
    var connected = false; // initially set connected to false
-   var getData = ' '; // initial set blank 
-
-   // Scratch UDP Listener (experimental) 
-   ext.cnct = function() {	
-   if (connected == false) {
-		server1.on("error", function (err) {
-			alert("server error:\n" + err.stack);
-			server.close();
-		});
-
-		server1.on("message", function (msg, rinfo) {
-			getData = ' '+msg+' '; 
-			});
-
-		server1.on("listening", function () {
-			myStatus = 2; });
-	    // listen on all IP adresses
-	    server1.bind(listenerPort,listenerHOST);
-		connected = true; 	
-	} else {
-		alert ("Scratch already listening on udp ports"); 
-	}
-   };		    
-  
-   // end UDP Listener (experimental)
-   
+     
    // Cleanup function when the extension is unloaded
 
    ext._shutdown = function() {
@@ -89,13 +62,10 @@
    
    // Send command and set Tello into SDK mode
    ext.sendcommand = function (callback) {
-   
+   myStatus = 2; // initially set status to yellow
    var message = new Buffer('command');
     
-     if (connected == false) {
-            alert("Tello not Connected! \n \n Establish WIFI connection first.");
-		}
-	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+ 	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
 		if (err) { 
 			alert("Command could not send: " + err);
 			client.close();
@@ -169,19 +139,6 @@
    });
    
    };
-   
-   // read Data improved for all Tello return codes, flight and state commands
-   ext.readData = function (val) {
-     
-     var message = new Buffer(val);
-	 //client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-	 //});
-	 		
-	 var test = getData.trim();
-     // to be implented split test 
-     return test;
-   };
-   
 
    // added function to support the Start The Program block
    ext.goGreen = function() {
@@ -192,9 +149,7 @@
    // Block and block menu descriptions
    var descriptor = {
     blocks: [
-		[' ', 'Receiver', 'cnct'],
 		[' ', 'Send command', 'sendcommand'],
-		['r', 'Read %m.readcommand', 'readData', 'speed?'],
 		[' ', 'take off', 'takeoff'],
 		[' ', 'land', 'land'],
 		[' ', 'fly %m.direction with distance %n', 'flydir', 'up', '20'],
@@ -205,13 +160,12 @@
  	  'menus': {
         'flipDirection': ['left', 'right', 'forward', 'backward'],
         'direction'    : ['up', 'down', 'forward', 'backward', 'left', 'right'],
-        'rotation'     : ['cw', 'ccw'],
-        'readcommand'  : ['speed?','battery?','time?','height?','temp?','attitude?','baro?','acceleration?','tof?']
+        'rotation'     : ['cw', 'ccw']
     },
     url: 'https://github.com/f41ardu/TelloforScratch',
     displayName: 'Tello SDK 1.3.0.0'
 };
 
    // Register the extension
-   ScratchExtensions.register('Tello SDK 0.1.0', descriptor, ext);
+   ScratchExtensions.register('Tello SDK 0.2.0', descriptor, ext);
  })({});
