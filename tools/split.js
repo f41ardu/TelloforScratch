e781913cff3db0e741b739f43dbb8b4e5bb98d26
tom <f41ardu@aror.de>
2019-06-20 07:53:02 +0200

    Fixed brocken version 0.6.0

--- tools/split.js
+++ tools/split.js
@@ -1,6 +1,9 @@
 var result;
-var string = "pitch:34;roll:-127;yaw:69;vgx:0;vgy:0;vgz:0;templ:81;temph:83;tof:145;h:0;bat:10;baro:1.06;time:2;agx:727.00;agy:434.00;agz:447.00;"
+var maximum = -145;
+var minimum = 145;
+var randomnumber = Math.round((Math.random() * (maximum - minimum + 1)) + minimum);
+var string = "pitch:34;roll:-127;yaw:69;vgx:0;vgy:0;vgz:0;templ:81;temph:83;tof:"+randomnumber+";h:0;bat:10;baro:1.06;time:2;agx:727.00;agy:434.00;agz:447.00;"
 
 // result = string.split(","); 
 var array = string.split(';').map(function (a) { return a.split(':'); });
-console.log(array[1][1]);
+console.log(array[8][1]);
